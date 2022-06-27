dbd5f31a06ada1d5d401bde0d4bd64920d954ad7
Ulrich Drepper <drepper@gmail.com>
2022-06-27 07:13:35 +0200

    Add run-qemu.sh

---
+++ run-qemu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+f=$(file ${@: -1})
+case "$f" in
+  *32-bit*UCB*) suf=riscv32 ;;
+  *64-bit*UCB*) suf=riscv64 ;;
+  *80386*) suf=i386 ;;
+  *x86-64*) suf=x86-64 ;;
+  *32-bit*ARM*) suf=arm ;;
+  *ARM*aarch64*) suf=aarch64 ;;
+  *) printf '???\n'; exit 1 ;;
+esac
+
+exec qemu-"$suf" -singlestep -d in_asm,cpu ./test "$@"
